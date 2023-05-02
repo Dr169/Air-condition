86093510ffe2cdaf1bab23b84a5fea00e25a85f5
Danial <daniradical69@gmail.com>
2023-05-02 09:35:45 +0430

    connect the backend ti signin page

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const httpLink = createHttpLink({
+  uri: "http://127.0.0.1:8000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <ApolloProvider client={client}>
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-  // </ApolloProvider>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 reportWebVitals();
